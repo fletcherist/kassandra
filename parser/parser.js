98d53a11732cb95653143abd63a8aee3930bbae9
phil romanov <romanovexports@gmail.com>
2016-06-20 00:34:11 +0300

    Add Some Filters

--- parser/parser.js
+++ parser/parser.js
@@ -91,13 +91,28 @@ var parseQuestion = (question) => {
   if (!question) {
     return false;
   }
-
+  if (question.match(/\?/)) {
+    return false;
+  }
+  if (question.match(/факты/ig)) {
+    return false;
+  }
+  if (question.match(/100%/ig)) {
+    return false;
+  }
+  if (question.match(/фото|фотку|онлайн|скрин/ig)) {
+    return false;
+  }
   question = question
     .replace('\n', '')
     .replace('<h1 class="streamItemContent streamItemContent-question">', '')
     .replace('</h1>', '')
     .replace(/^\s{0,}/, '')
-    .replace(/\s{0,}$/, '');
+    .replace(/\s{0,}$/, '')
+    .replace(/&quot;/, ' ')
+    .replace('\n', '')
+    .replace(/&lt;/, '<')
+    .replace(/<a class="questionersName" .{0,}<\/a>/, '');
   return question;
 };
 
